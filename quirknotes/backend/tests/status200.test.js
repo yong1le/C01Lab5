b29223dd9434e99009213b52e1050d8811e2b024
yong1le <ygle.he@gmail.com>
2024-02-14 09:51:42 -0500

    init

---
+++ quirknotes/backend/tests/status200.test.js
@@ -0,0 +1,27 @@
+test("1+2=3, empty array is empty", () => {
+  expect(1 + 2).toBe(3);
+  expect([].length).toBe(0);
+});
+
+const SERVER_URL = "http://localhost:4000";
+
+test("/postNote - Post a note", async () => {
+  const title = "NoteTitleTest";
+  const content = "NoteTitleContent";
+
+  const postNoteRes = await fetch(`${SERVER_URL}/postNote`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title,
+      content: content,
+    }),
+  });
+
+  const postNoteBody = await postNoteRes.json();
+
+  expect(postNoteRes.status).toBe(200);
+  expect(postNoteBody.response).toBe("Note added succesfully.");
+});
